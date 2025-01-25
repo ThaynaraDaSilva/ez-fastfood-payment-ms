4fe2f1dc093ca29541d504ddb0723dbbbd52ba25
tchfer <tchfernando@gmail.com>
2025-01-25 14:59:59 -0300

    Ajustar campo de customer_id para user_id

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA IF NOT EXISTS EZ_FASTFOOD_PAYMENT;
 
 CREATE TABLE IF NOT EXISTS EZ_FASTFOOD_PAYMENT.PAYMENT (
     id BIGSERIAL PRIMARY KEY,
-    order_id INT,
-    customer_id INT NULL,
+    order_id INT NOT NULL,
+    user_id INT NULL,
     payment_date TIMESTAMP WITH TIME ZONE NULL,
     payment_price DECIMAL,
     payment_status VARCHAR(50)
